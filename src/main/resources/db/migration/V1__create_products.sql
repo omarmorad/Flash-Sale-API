7768b05814ee522ce68c88f653488be4d147902e
Omar Morad <omorad5362@gmail.com>
2026-06-05 23:16:03 +0300

    explanantion  why >= was applied  on stock column

--- src/main/resources/db/migration/V1__create_products.sql
+++ src/main/resources/db/migration/V1__create_products.sql
@@ -4,7 +4,7 @@ CREATE TABLE products(
     description TEXT,
     original_price NUMERIC (10,2) NOT NULL,
     sale_price NUMERIC(10,2) NOT NULL,
-    stock INTEGER NOT NULL CHECK(stock >= 0),
+    stock INTEGER NOT NULL CHECK(stock >= 0), --here this is just guard for and edge case that came through my mind where i thought about ..what about if my code goes wrong and i done some missy logic which lead to overselling..shouldnt iprevent that from db level?? i think yes on every level we should apply the prevention as long as we can do it 
 
-    
+
 )
