041b1f059ad42e3c04ee4b0929009afd62945d5e
Omar Morad <omorad5362@gmail.com>
2026-06-05 19:10:21 +0300

    style: fix indentation and add docker compose for local dev

--- src/main/java/com/flashsale/FlashSaleApiApplication.java
+++ src/main/java/com/flashsale/FlashSaleApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FlashSaleApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlashSaleApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FlashSaleApiApplication.class, args);
+    }
 
 }
