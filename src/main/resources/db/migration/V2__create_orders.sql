f7da42f652151ee64c0f2b516a7f0efc3e50cc95
Omar Morad <omorad5362@gmail.com>
2026-06-06 00:09:49 +0300

    the smart fella autocomplete helped with this but i thnk that smart guy is missing somethng..investigating then back again

---
+++ src/main/resources/db/migration/V2__create_orders.sql
@@ -0,0 +1,10 @@
+CREATE TABLE orders (
+    id               BIGSERIAL PRIMARY KEY,
+    product_id       BIGINT NOT NULL REFERENCES products(id),
+    user_id          BIGINT NOT NULL,
+    idempotency_key  VARCHAR(255) NOT NULL UNIQUE,       -- preventing  duplicate orders on retry
+    quantity         INTEGER NOT NULL DEFAULT 1,
+    purchase_price   NUMERIC(10, 2) NOT NULL,
+    status           VARCHAR(50) NOT NULL DEFAULT 'CONFIRMED',
+    created_at       TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
