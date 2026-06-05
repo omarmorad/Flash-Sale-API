f39a8e77cc17a8fbd3318592d367edf8dc4b1d04
Omar Morad <omorad5362@gmail.com>
2026-06-05 23:27:48 +0300

    adding timeframe for active sales price ranges

--- src/main/resources/db/migration/V1__create_products.sql
+++ src/main/resources/db/migration/V1__create_products.sql
@@ -7,4 +7,9 @@ CREATE TABLE products(
     stock INTEGER NOT NULL CHECK(stock >= 0), --here this is just guard for and edge case that came through my mind where i thought about ..what about if my code goes wrong and i done some missy logic which lead to overselling..shouldnt iprevent that from db level?? i think yes on every level we should apply the prevention as long as we can do it 
 
 
+    sale_start TIMESTAMP NOT NULL, --why have we addd this is beacuse wanna represent the active timeframe for a product flash sale pricin
+    sale_end TIMESTAMP NOT NULL, --so if its larger than this end the product is now not eligiible for that specifc sale_price
+
+    
+
 )
