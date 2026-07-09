0e826c60f7e8752677e7a35f0667d19073b264d9
Omar Morad <omorad5362@gmail.com>
2026-07-09 16:39:51 +0300

    feat(flashsale-api): add caching and debug print
    add @EnableCaching annotation and its required import to enable Spring's caching framework, and add a temporary debug print statement in the main method to test application startup

--- src/main/java/com/flashsale/FlashSaleApiApplication.java
+++ src/main/java/com/flashsale/FlashSaleApiApplication.java
@@ -2,12 +2,14 @@ package com.flashsale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class FlashSaleApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FlashSaleApiApplication.class, args);
+        System.out.println("omar");
     }
 
 }
